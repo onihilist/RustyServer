358a25af18622c30c868f9c2789d012c59d40994
o.nihilist <107046146+onihilist@users.noreply.github.com>
2024-05-30 18:55:45 +0200

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
+mod utils;
+
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener, TcpStream};
+use crate::utils::Logs::UtilsData;
 
 fn handler(stream: TcpStream) {
-    println!("Someone connected.");
+    let mut user: SocketAddr = stream.local_addr().unwrap();
+    let msgUser: &str = format!("{} has connected successfully", user).as_str();
+    let mut logs: UtilsData = utils::Logs::initLog(None, msgUser, None);
+    utils::Logs::info(logs);
 }
 
 fn main() -> std::io::Result<()> {
+    let mut logs: UtilsData = utils::Logs::initLog(None, "Server is starting...", None);
+    utils::Logs::info(logs);
     let addr: IpAddr = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
     let port: u16 = 42000;
     let socket_addr = SocketAddr::from(SocketAddr::new(addr, port));
     let listener: TcpListener = TcpListener::bind(socket_addr)?;
-    println!("Listening for a connection...");
+    let mut logs: UtilsData = utils::Logs::initLog(None, "Server is successfully started !", None);
+    utils::Logs::success(logs);
+    let mut logs: UtilsData = utils::Logs::initLog(None, "Listening for a connection...", None);
+    utils::Logs::info(logs);
 
     for stream in listener.incoming() {
         handler(stream?);
